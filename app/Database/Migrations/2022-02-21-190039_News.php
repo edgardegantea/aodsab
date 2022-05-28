8ba543b6ed17a100fa58a8bb93ecb01e69ecc633
edgardegantea <edgar.degante.a@gmail.com>
2022-05-28 17:28:11 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/Config/Routes.php
    #	app/Controllers/NewsController.php
    #	app/Database/Migrations/2022-02-21-190039_News.php
    #	app/Database/Migrations/2022-03-26-224039_Staff.php
    #	app/Views/layout/navbar.php
    #	app/Views/news/create.php
    #	app/Views/news/index.php

--- app/Database/Migrations/2022-02-21-190039_News.php
+++ app/Database/Migrations/2022-02-21-190039_News.php
@@ -8,28 +8,26 @@ class News extends Migration
 {
     public function up()
     {
+        $this->db->disableForeignKeyChecks();
+
         $this->forge->addField([
-            'id'    => [
-                'type'              => 'INT',
-                'constraint'        => 12,
-                'unsigned'          => true,
-                'auto_increment'    => true
-            ],
-            'title' => [
-                'type'              => 'VARCHAR',
-                'constraint'        => 255
-            ],
-            'slug'  => [
-                'type'              => 'VARCHAR',
-                'constraint'        => 255
-            ],
-            'body'  => [
-                'type'              => 'TEXT'
-            ]
-        ]);  
+            'id'            => ['type' => 'INT', 'constraint'  => 12, 'unsigned' => true, 'auto_increment' => true],
+            'periodista'    => ['type' => 'int', 'unsigned' => true, 'constraint' => 12, 'auto_increment' => false],
+            'title'         => ['type' => 'VARCHAR', 'constraint' => 255],
+            'slug'          => ['type' => 'VARCHAR', 'constraint' => 255],
+            'body'          => ['type' => 'TEXT'],
+            'categoria'     => ['type' => 'int', 'unsigned' => true, 'constraint' => 12, 'auto_increment' => false],
+            'staff'         => ['type' => 'int', 'unsigned' => true, 'constraint' => 12, 'auto_increment' => false]
+        ]);
 
         $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('periodista', 'periodistas', 'id');
+        $this->forge->addForeignKey('categoria', 'categorias', 'id');
+        $this->forge->addForeignKey('staff', 'staffs', 'id');
+
         $this->forge->createTable('news', true);
+
+        $this->db->enableForeignKeyChecks();
     }
 
     public function down()
